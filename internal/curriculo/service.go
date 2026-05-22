4f39d57d76903bb3b0661fb8cb494082c5383f54
berntau <tbptffc@gmail.com>
2026-05-22 15:17:37 -0300

    feat: adiciona método que retorna curriculo armazenado

--- internal/curriculo/service.go
+++ internal/curriculo/service.go
@@ -12,3 +12,10 @@ func NewResumeService() *ResumeService {
 		resume: NewResume(),
 	}
 }
+
+// GetResume retorna o currículo armazenado no serviço
+// Note: método em Go é uma função com "receiver" — o (s *ResumeService) antes do nome
+// É assim que Go faz "orientação a objetos" sem classes
+func (s *ResumeService) GetResume() Resume {
+	return s.resume
+}
